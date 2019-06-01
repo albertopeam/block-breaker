b622d5df96fe2613e99fbdd7aaa9bdbe662b57ce
Alberto Penas Amor <alberto.penas.amor@mobgen.com>
2019-06-01 18:04:53 +0200

    GameStatus added as singleton

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -10,6 +10,16 @@ public class GameStatus : MonoBehaviour {
     [SerializeField] TextMeshProUGUI scoreText;
     private int score = 0;
 
+    private void Awake() {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1) {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     private void Start() {
         UpdateScoreText();
     }
