348574cfb80f63c662aee9fd0ccb248ff1eefc88
Alberto Penas Amor <albertopeam@gmail.com>
2019-08-18 09:33:11 +0200

    add random factor to Ball Rigibody velocity when colliding with stuff

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,11 +6,15 @@ public class Ball : MonoBehaviour {
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 10f;
     [SerializeField] AudioClip[] collisionClips;
+    [SerializeField] float randomFactor = 0.2f;
     private Vector2 paddleToBallVector;
     private bool launched;
+    private Rigidbody2D myRigidbody;
+
 
     // Start is called before the first frame update
     void Start() {
+        myRigidbody = GetComponent<Rigidbody2D>();
         paddleToBallVector = transform.position - paddle.transform.position;
         launched = false;
     }
@@ -30,17 +34,19 @@ public class Ball : MonoBehaviour {
 
     private void LaunchOnMouseClick() {
         if (Input.GetMouseButtonDown(0)) {
-            Rigidbody2D rigibody = GetComponent<Rigidbody2D>();
-            rigibody.velocity = new Vector2(xPush, yPush);
+            myRigidbody.velocity = new Vector2(xPush, yPush);
             launched = true;
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D collision) {                
         if (launched) {
             int item = Random.Range(0, collisionClips.Length);
             AudioClip clip = collisionClips[item];
             GetComponent<AudioSource>().PlayOneShot(clip);
+
+            Vector2 velocityTweak = new Vector2(Random.Range(0f, randomFactor), Random.Range(0f, randomFactor));
+            myRigidbody.velocity += velocityTweak;
         }
     }
 }
